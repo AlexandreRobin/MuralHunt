1a552bd5352d62875afd23c6edddca2ed1011684
AlexandreRobin <robinalexandre13@gmail.com>
2024-04-24 22:11:41 +0200

    Add mural festival data and update geolocator

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,9 +1,10 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:xml/xml.dart';
 import 'package:muralhunt/utils/mural.dart';
 
 class API {
-  static Future<List<dynamic>> fetchData() async {
+  static Future<List<dynamic>> fetchMontrealData() async {
     var url = Uri.https(
         'donnees.montreal.ca', '/api/3/action/datastore_search', {
       'resource_id': 'f02401c2-8336-4086-9955-4c5592ace72e',
@@ -18,8 +19,23 @@ class API {
     }
   }
 
-  static Future<List<Mural>> getMurals() async {
-    List<dynamic> data = await fetchData();
+  static Future<XmlDocument> fetchMuralFestivalData() async {
+    var url = Uri.https(
+        'www.google.com', '/maps/d/kml', {
+      'forcekml': '1',
+      'mid': '1oKlF5bEL1Boq1qKjpNmcgBE5jenYdP4'
+    });
+
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      return XmlDocument.parse(response.body);
+    } else {
+      throw Exception('Failed to fetch data');
+    }
+  }
+
+  static Future<List<Mural>> getMontrealMurals() async {
+    List<dynamic> data = await fetchMontrealData();
     List<Future<Mural>> futureMurals = data.map((record) async {
       Mural mural = Mural(
           id: record['id'],
@@ -35,6 +51,32 @@ class API {
       return await mural.getCapture();
     }).toList();
     List<Mural> murals = await Future.wait(futureMurals);
+    return murals;
+  }
+
+  static getFestivalMurals() async {
+    XmlDocument data = await fetchMuralFestivalData();
+    List<Future<Mural>> futureMurals = data.findAllElements('Placemark').map((placemark) async {
+      String image = placemark.getElement('ExtendedData')!.findAllElements('Data')
+        .where((element) => element.attributes.any((attribute) => attribute.name.local == 'name' && attribute.value == 'gx_media_links'))
+        .first.innerText.replaceAll(' ', '').replaceAll('\n', '');
+      List<String> coordinates = placemark.getElement('Point')!.getElement('coordinates')!.innerText.split(',');
+      Mural mural = Mural(
+          id: image.substring(image.length - 16),
+          artiste: placemark.getElement('name')!.innerText,
+          latitude: double.parse(coordinates[1]),
+          longitude: double.parse(coordinates[0].replaceAll(' ', '').replaceAll('\n', '')),
+          image: image);
+      return await mural.getCapture();
+    }).toList();
+    List<Mural> murals = await Future.wait(futureMurals);
+    return murals;
+  }
+
+  static Future<List<Mural>> getMurals() async {
+    List<Mural> montrealMurals = await getMontrealMurals();
+    List<Mural> festivalMurals = await getFestivalMurals();
+    List<Mural> murals = montrealMurals + festivalMurals;
     return murals;
   }
 }
